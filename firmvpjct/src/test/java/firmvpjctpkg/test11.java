5f70ae2280bc9dd2658eb6a7768262b268051893
himajosee <himajosee@gmail.com>
2024-12-13 12:36:20 +0530

    Add files via upload

---
+++ firmvpjct/src/test/java/firmvpjctpkg/test11.java
@@ -0,0 +1,68 @@
+package firmvpjctpkg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class test11 {
+
+	WebDriver driver;
+	
+	@Test(dataProvider="data")
+	
+	public void h1(String user,String pass) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Downloads\\chromedriver-win64 (3)\\chromedriver-win64\\chromedriver.exe");
+		 
+		 driver=new ChromeDriver();
+			
+	     driver.get("https://www.saucedemo.com/v1/");
+	     
+	     WebElement uname=driver.findElement(By.id("user-name"));
+	     uname.sendKeys(user);
+	 	
+	 	
+	 	WebElement pwd1=driver.findElement(By.id("password"));
+	 	pwd1.sendKeys(pass);
+	 	
+	    WebElement loginbtn1=driver.findElement(By.id("login-button"));
+	 	loginbtn1.click();
+	    
+			
+	}
+	
+	
+	
+	
+	
+	@AfterTest
+	public void close() {
+		
+		driver.quit();
+		
+	}
+	
+	
+	
+	
+	@DataProvider(name="data")
+	
+	public Object[][]data() {
+		
+		return new Object[][] {
+			{"user1","pass1"},
+			{"standard_user","secret_sauce"}
+			
+		};
+		
+		
+	} 
+	
+	
+	
+	
+}
